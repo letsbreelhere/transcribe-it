0ffc36e4c801c408766e8cfe7c90044065aaa6a3
Bree Elle Gardner <letsbreelhere@gmail.com>
2021-02-21 09:04:32 -0800

    Cleaning up the worklet

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs';
 import App from './App';
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)({ latencyHint: "playback" });
-const processorPath = 'static/StretchProcessor.tsx';
+const processorPath = 'static/StretchProcessor.js';
 const processorSource = readFileSync(processorPath); // just a promisified version of fs.readFile
 const processorBlob = new Blob([processorSource.toString()], { type: 'text/javascript' });
 const processorURL = URL.createObjectURL(processorBlob);
--- static/StretchProcessor.js
+++ static/StretchProcessor.js
@@ -1,30 +1,31 @@
+// eslint-disable-next-line max-classes-per-file
 class RingBuffer {
   constructor(size) {
     this.size = size;
     this.buffer = [];
     this.writeCursor = 0;
     this.readCursor = 0;
-    this.inBuffer;
+    this.inBuffer = 0;
   }
 
-  push(values) {
+  enq(values) {
     if (this.size - this.inBuffer < values.length) {
-      throw "Buffer full";
+      throw new Error('Buffer full');
     }
     this.inBuffer += values.length;
 
-    values.forEach((val) => {
-      this.buffer[this.writeCursor++ % size] = value;
-    })
+    values.forEach((value) => {
+      this.buffer[this.writeCursor++ % this.size] = value;
+    });
   }
 
-  take(n) {
-    let res = [];
+  deq(n) {
+    const res = [];
     for (let i = 0; i < n; i++) {
-      if (this.inBuffer === 0) {
-        throw "Buffer empty";
+      if (this.inBuffer <= 0) {
+        throw new Error('Buffer empty');
       }
-      res.push(this.buffer[this.readCursor++ % size]);
+      res.push(this.buffer[this.readCursor++ % this.size]);
     }
 
     return res;
@@ -37,7 +38,7 @@ class StretchProcessor extends AudioWorkletProcessor {
   }
 
   constructor(...opts) {
-    super(...opts)
+    super(...opts);
     this.lbuf = [];
     this.rbuf = [];
     this.ix = null;
@@ -47,7 +48,7 @@ class StretchProcessor extends AudioWorkletProcessor {
     const rate = 2;
 
     if (this.ix === null) {
-      for (let i = 0; i < 128; ++i) {
+      for (let i = 0; i < 128; i++) {
         outputs[0][0][i] = inputs[0][0][i];
         outputs[0][1][i] = inputs[0][1][i];
       }
@@ -55,15 +56,15 @@ class StretchProcessor extends AudioWorkletProcessor {
       this.rbuf = Array(inputs[0][1]);
       this.ix = 0;
     } else {
-      for (let i = 0; i < 128; ++i) {
-        outputs[0][0][i] = this.lbuf[this.ix+i];
-        outputs[0][1][i] = this.rbuf[this.ix+i];
+      for (let i = 0; i < 128; i++) {
+        outputs[0][0][i] = this.lbuf[this.ix + i];
+        outputs[0][1][i] = this.rbuf[this.ix + i];
       }
       this.ix += 128;
-      const writeIx = rate*this.ix;
-      for (let i = 0; i < rate*128; ++i) {
-        this.lbuf[writeIx + i] = inputs[0][0][i%128];
-        this.rbuf[writeIx + i] = inputs[0][1][i%128];
+      const writeIx = rate * this.ix;
+      for (let i = 0; i < rate * 128; i++) {
+        this.lbuf[writeIx + i] = inputs[0][0][i % 128];
+        this.rbuf[writeIx + i] = inputs[0][1][i % 128];
       }
     }
     return true;
